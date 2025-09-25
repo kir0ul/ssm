6f42a86b3f8fdfe4a76cdebe0d042bee3351c068
Andrea Pierré <andrea_pierre@brown.edu>
2025-09-25 15:21:11 -0400

    fix: warning

--- ssm/observations.py
+++ ssm/observations.py
@@ -940,7 +940,7 @@ class _AutoRegressiveObservationsBase(Observations):
     Base class for autoregressive observations of the form,
 
     E[x_t | x_{t-1}, z_t=k, u_t]
-        = \sum_{l=1}^{L} A_k^{(l)} x_{t-l} + b_k + V_k u_t.
+        = \\sum_{l=1}^{L} A_k^{(l)} x_{t-l} + b_k + V_k u_t.
 
     where L is the number of lags and u_t is the input.
     """
